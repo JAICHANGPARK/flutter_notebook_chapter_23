895f7ae9c48e550f01c97eb8738dbc19ced2613c
Dreamwalker <aristojeff@gmail.com>
2022-10-11 21:28:47 +0900

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_notebook_chapter_23/main.dart';
+import 'package:flutter_notebook_chapter_23/ep1301_smart_home_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
