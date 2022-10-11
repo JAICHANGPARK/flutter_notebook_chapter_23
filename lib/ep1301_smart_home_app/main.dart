5dfd3334fe7ace0014b5d6750ea38fdfc3b91805
Dreamwalker <aristojeff@gmail.com>
2022-10-11 21:30:34 +0900

    Update main.dart

--- lib/ep1301_smart_home_app/main.dart
+++ lib/ep1301_smart_home_app/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_23/ep1301_smart_home_app/src/ui/smart_home_app.dart';
 
 
 main() {
-  runApp(Container());
+  runApp(SmartHomeApp());
 }
