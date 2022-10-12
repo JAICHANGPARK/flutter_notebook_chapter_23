c25abb516cf70975a4486df2b1db183078b7fa55
Dreamwalker <aristojeff@gmail.com>
2022-10-12 17:27:46 +0900

    Update smart_home_main_page.dart

--- lib/ep1301_smart_home_app/src/ui/smart_home_main_page.dart
+++ lib/ep1301_smart_home_app/src/ui/smart_home_main_page.dart
@@ -11,6 +11,7 @@ class _SmartHomeMainPageState extends State<SmartHomeMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           children: [
