d55578288f0f21aef635390d5ad29a398a15e753
Dreamwalker <aristojeff@gmail.com>
2022-10-13 18:39:55 +0900

    Create smart_lighting_page.dart

---
+++ lib/ep1301_smart_home_app/src/ui/smart_lighting_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SmartLightingPage extends StatefulWidget {
+  const SmartLightingPage({Key? key}) : super(key: key);
+
+  @override
+  State<SmartLightingPage> createState() => _SmartLightingPageState();
+}
+
+class _SmartLightingPageState extends State<SmartLightingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
