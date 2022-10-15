aff52c76fea825e52d218feedd901f299d44a108
Dreamwalker <aristojeff@gmail.com>
2022-10-15 11:04:13 +0900

    Create ecommerce_main_page.dart

---
+++ lib/ep1305_ecommerce_app/src/ui/ecommerce_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EcommerceMainPage extends StatefulWidget {
+  const EcommerceMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<EcommerceMainPage> createState() => _EcommerceMainPageState();
+}
+
+class _EcommerceMainPageState extends State<EcommerceMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
