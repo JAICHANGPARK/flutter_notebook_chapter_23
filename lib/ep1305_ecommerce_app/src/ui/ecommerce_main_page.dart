6feb777eb10ddf8598f36b98ee32be923439b4ea
Dreamwalker <aristojeff@gmail.com>
2022-10-15 11:07:42 +0900

    Update ecommerce_main_page.dart

--- lib/ep1305_ecommerce_app/src/ui/ecommerce_main_page.dart
+++ lib/ep1305_ecommerce_app/src/ui/ecommerce_main_page.dart
@@ -8,15 +8,23 @@ class EcommerceMainPage extends StatefulWidget {
 }
 
 class _EcommerceMainPageState extends State<EcommerceMainPage> {
+  int _pageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _pageIndex,
+        onTap: (idx) {
+          setState(() {
+            _pageIndex = idx;
+          });
+        },
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Favorite"),
           BottomNavigationBarItem(icon: Icon(Icons.notifications_none), label: "Notifications"),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Favorite"),
+          BottomNavigationBarItem(icon: Icon(Icons.perm_identity), label: "Profile"),
         ],
       ),
     );
