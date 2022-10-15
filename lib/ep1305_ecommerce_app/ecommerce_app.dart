dafee072022643d13d9fab048039a74e2a302199
Dreamwalker <aristojeff@gmail.com>
2022-10-15 11:04:59 +0900

    Update ecommerce_app.dart

--- lib/ep1305_ecommerce_app/ecommerce_app.dart
+++ lib/ep1305_ecommerce_app/ecommerce_app.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_23/ep1305_ecommerce_app/src/ui/ecommerce_main_page.dart';
+import 'package:go_router/go_router.dart';
 
 class EcommerceApp extends StatelessWidget {
-  const EcommerceApp({Key? key}) : super(key: key);
+  EcommerceApp({Key? key}) : super(key: key);
+  final _router = GoRouter(routes: [
+    GoRoute(
+      path: "/",
+      builder: (context, state) => EcommerceMainPage(),
+    ),
+  ], initialLocation: "/");
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      
+      routerConfig: _router,
     );
   }
 }
