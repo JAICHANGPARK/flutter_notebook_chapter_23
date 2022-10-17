c7ff35e89fbabe41f644828dc270f5a9580afa00
Dreamwalker <aristojeff@gmail.com>
2022-10-17 17:03:39 +0900

    Update ecommerce_main_page.dart

--- lib/ep1305_ecommerce_app/src/ui/ecommerce_main_page.dart
+++ lib/ep1305_ecommerce_app/src/ui/ecommerce_main_page.dart
@@ -40,11 +40,11 @@ class _EcommerceMainPageState extends State<EcommerceMainPage> {
         },
         type: BottomNavigationBarType.fixed,
         selectedItemColor: Colors.blue,
-        items: [
-          const BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
-          const BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Favorite"),
-          const BottomNavigationBarItem(icon: Icon(Icons.notifications_none), label: "Notifications"),
-          const BottomNavigationBarItem(icon: Icon(Icons.perm_identity), label: "Profile"),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Favorite"),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications_none), label: "Notifications"),
+          BottomNavigationBarItem(icon: Icon(Icons.perm_identity), label: "Profile"),
         ],
       ),
     );
