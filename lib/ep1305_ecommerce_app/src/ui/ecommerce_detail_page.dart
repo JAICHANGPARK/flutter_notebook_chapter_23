8ddf2fd79c587f1093f1409da12f2c55ee483e2e
Dreamwalker <aristojeff@gmail.com>
2022-10-18 19:25:44 +0900

    Update ecommerce_detail_page.dart

--- lib/ep1305_ecommerce_app/src/ui/ecommerce_detail_page.dart
+++ lib/ep1305_ecommerce_app/src/ui/ecommerce_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class EcommerceDetailPage extends StatefulWidget {
   const EcommerceDetailPage({Key? key}) : super(key: key);
 
@@ -11,6 +10,18 @@ class EcommerceDetailPage extends StatefulWidget {
 class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Divider(
+            height: 0,
+            color: Colors.black,
+          ),
+          Container(
+            height: 72,
+          )
+        ],
+      ),
+    );
   }
 }
