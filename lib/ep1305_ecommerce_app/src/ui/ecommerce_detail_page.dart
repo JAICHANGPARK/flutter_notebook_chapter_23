0babf6a3b3a9543edccaa34913f6d5ad606d4806
Dreamwalker <aristojeff@gmail.com>
2022-10-18 19:35:24 +0900

    Update ecommerce_detail_page.dart

--- lib/ep1305_ecommerce_app/src/ui/ecommerce_detail_page.dart
+++ lib/ep1305_ecommerce_app/src/ui/ecommerce_detail_page.dart
@@ -11,75 +11,77 @@ class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                height: 48,
-                width: 48,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                  border: Border.all(),
-                ),
-                child: IconButton(
-                  icon: const Icon(
-                    Icons.arrow_back,
-                  ),
-                  onPressed: () {},
-                ),
-              ),
-              Expanded(child: Text("Product details")),
-            ],
-          ),
-          const Expanded(child: Placeholder()),
-          const Divider(
-            height: 0,
-            color: Colors.black,
-          ),
-          Container(
-            height: 80,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.lightBlue,
-                borderRadius: BorderRadius.circular(36),
-              ),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 18,
-              ),
-              child: Row(
-                children: const [
-                  Icon(
-                    Icons.lock,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 48,
+                  width: 48,
+                  decoration: BoxDecoration(
                     color: Colors.white,
+                    shape: BoxShape.circle,
+                    border: Border.all(),
                   ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    "Add item to bag",
-                    style: TextStyle(
-                      color: Colors.white,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back,
                     ),
+                    onPressed: () {},
                   ),
-                  Spacer(),
-                  Text(
-                    "\$549.00",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+                Expanded(child: Text("Product details")),
+              ],
             ),
-          )
-        ],
+            const Expanded(child: Placeholder()),
+            const Divider(
+              height: 0,
+              color: Colors.black,
+            ),
+            Container(
+              height: 80,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(36),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 18,
+                ),
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.lock,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      "Add item to bag",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      "\$549.00",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
