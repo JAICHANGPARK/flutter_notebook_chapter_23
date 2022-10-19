971707cb5c1025e7eb615c7af06b833ade34e1b1
Dreamwalker <aristojeff@gmail.com>
2022-10-19 18:22:19 +0900

    Update ecommerce_detail_page.dart

--- lib/ep1305_ecommerce_app/src/ui/ecommerce_detail_page.dart
+++ lib/ep1305_ecommerce_app/src/ui/ecommerce_detail_page.dart
@@ -300,7 +300,7 @@ class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
                                 const Expanded(
                                   child: TextField(
                                     decoration: InputDecoration(
-                                      
+                                      border: InputBorder.none,
                                     ),
                                   ),
                                 ),
