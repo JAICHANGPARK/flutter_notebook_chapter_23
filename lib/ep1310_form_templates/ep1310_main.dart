c7427c4652b8037ff642009df6abf603cb844870
Dreamwalker <aristojeff@gmail.com>
2022-10-20 16:31:25 +0900

    Update ep1310_main.dart

--- lib/ep1310_form_templates/ep1310_main.dart
+++ lib/ep1310_form_templates/ep1310_main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_notebook_chapter_23/ep1310_form_templates/forms_templetes_app.dart';
 
 main() {
-  runApp(const FormsTemplatesApp());
+  runApp(FormsTemplatesApp());
 }
