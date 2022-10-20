98c0a567dc7fe48da81c1d9288a7137a06ebd9f9
Dreamwalker <aristojeff@gmail.com>
2022-10-20 16:31:19 +0900

    Create templates_home_page.dart

---
+++ lib/ep1310_form_templates/src/ui/templates_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TemplatesHomePage extends StatefulWidget {
+  const TemplatesHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<TemplatesHomePage> createState() => _TemplatesHomePageState();
+}
+
+class _TemplatesHomePageState extends State<TemplatesHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
