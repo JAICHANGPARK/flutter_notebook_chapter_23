0160112e3bd9b7d5c5f89ae838ac1b4094d3748d
Dreamwalker <aristojeff@gmail.com>
2022-10-21 17:06:49 +0900

    Update billing_template_page.dart

--- lib/ep1310_form_templates/src/ui/billing_template_page.dart
+++ lib/ep1310_form_templates/src/ui/billing_template_page.dart
@@ -12,26 +12,47 @@ class _BillingTemplatePageState extends State<BillingTemplatePage> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.only(left: 16, right: 16, top: 48),
+        padding: const EdgeInsets.only(left: 8, right: 8, top: 58),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Card(
               color: Colors.white,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+              surfaceTintColor: Colors.white,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Billing"),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Plan",
-                        hintText: "Annual Plan",
-                        border: OutlineInputBorder(),
-                        enabledBorder: OutlineInputBorder(),
-
+                    Text(
+                      "Billing",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
                       ),
                     ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 12),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: "Plan",
+                          hintText: "Annual Plan",
+                          border: OutlineInputBorder(),
+                          enabledBorder: OutlineInputBorder(),
+                          suffixIcon: Icon(
+                            Icons.arrow_drop_down,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Due in 30 days"),
+                        Text("\$99.95"),
+                      ],
+                    )
                   ],
                 ),
               ),
