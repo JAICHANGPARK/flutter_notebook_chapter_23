283733adb68d04cbd5917cfc3ea676d191879645
Dreamwalker <aristojeff@gmail.com>
2022-10-21 17:02:12 +0900

    Update billing_template_page.dart

--- lib/ep1310_form_templates/src/ui/billing_template_page.dart
+++ lib/ep1310_form_templates/src/ui/billing_template_page.dart
@@ -11,17 +11,33 @@ class _BillingTemplatePageState extends State<BillingTemplatePage> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Card(
-            child: Column(
-              children: [
-                Text("Billing"),
-              ],
-            ),
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16, top: 48),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text("Billing"),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Plan",
+                        hintText: "Annual Plan",
+                        border: OutlineInputBorder(),
+                        enabledBorder: OutlineInputBorder(),
+
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
