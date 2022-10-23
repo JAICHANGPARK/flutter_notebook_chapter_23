8efb028d5e76e87540d995be77280fda8606142d
Dreamwalker <aristojeff@gmail.com>
2022-10-23 10:53:48 +0900

    Update ufc_home_page.dart

---
+++ lib/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NewUfcHomePage extends StatefulWidget {
+  const NewUfcHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<NewUfcHomePage> createState() => _NewUfcHomePageState();
+}
+
+class _NewUfcHomePageState extends State<NewUfcHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
