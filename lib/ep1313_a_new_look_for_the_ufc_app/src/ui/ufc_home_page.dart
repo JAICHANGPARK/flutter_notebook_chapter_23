bcee3820d83f5f63bad26e72883b1b54bd53b57b
Dreamwalker <aristojeff@gmail.com>
2022-10-23 11:02:40 +0900

    Update ufc_home_page.dart

--- lib/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_home_page.dart
+++ lib/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class NewUfcHomePage extends StatefulWidget {
   const NewUfcHomePage({Key? key}) : super(key: key);
 
@@ -12,19 +11,82 @@ class _NewUfcHomePageState extends State<NewUfcHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  child: Text("Logo"),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: const Text(
+                        "Logo",
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.menu,
+                      ),
+                    )
+                  ],
                 ),
-                IconButton(onPressed: (){}, icon: Icon(Icons.menu))
-              ],
-            )
-          ],
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  "STACKED CARD",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                height: 240,
+                color: Colors.red,
+              ),
+              Container(
+                height: 100,
+                color: Colors.pink,
+              ),
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    child: Text(
+                      "WATCH EMBEDDED SERIES",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          child: Container(
+                            height: 64,
+                            color: Colors.red,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ))
+            ],
+          ),
         ),
       ),
     );
