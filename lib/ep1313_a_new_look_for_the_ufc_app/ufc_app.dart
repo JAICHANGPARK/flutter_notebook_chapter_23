9a1a10b2f2e95f729c3974e92c369b24308b901d
Dreamwalker <aristojeff@gmail.com>
2022-10-23 10:55:48 +0900

    Update ufc_app.dart

--- lib/ep1313_a_new_look_for_the_ufc_app/ufc_app.dart
+++ lib/ep1313_a_new_look_for_the_ufc_app/ufc_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_23/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_home_page.dart';
 import 'package:go_router/go_router.dart';
 
 class NewUfcApp extends StatelessWidget {
@@ -6,7 +7,14 @@ class NewUfcApp extends StatelessWidget {
 
   final _router = GoRouter(
     initialLocation: "/",
-    routes: [],
+    routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) {
+          return const NewUfcHomePage();
+        },
+      ),
+    ],
   );
 
   @override
