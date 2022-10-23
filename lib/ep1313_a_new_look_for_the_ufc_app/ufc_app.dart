e359e4097dbaca980d2c357cfb9eba1430c85526
Dreamwalker <aristojeff@gmail.com>
2022-10-23 10:52:55 +0900

    Create ufc_app.dart

---
+++ lib/ep1313_a_new_look_for_the_ufc_app/ufc_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class NewUfcApp extends StatelessWidget {
+  const NewUfcApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
