5abf4b320c0e7e6e675456cff62f34c4ac4b7088
Dreamwalker <aristojeff@gmail.com>
2022-10-25 16:52:12 +0900

    Create ufc_detail_page.dart

---
+++ lib/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class UfcDetailPage extends StatefulWidget {
+  const UfcDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<UfcDetailPage> createState() => _UfcDetailPageState();
+}
+
+class _UfcDetailPageState extends State<UfcDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
