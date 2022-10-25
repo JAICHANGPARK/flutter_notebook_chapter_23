8dffcae6580f62e3de15043a6b2f809e2897320f
Dreamwalker <aristojeff@gmail.com>
2022-10-25 16:57:58 +0900

    Update ufc_detail_page.dart

--- lib/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_detail_page.dart
+++ lib/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_detail_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_23/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_appbar_widget.dart';
 
-
 class UfcDetailPage extends StatefulWidget {
   const UfcDetailPage({Key? key}) : super(key: key);
 
@@ -13,10 +12,23 @@ class _UfcDetailPageState extends State<UfcDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const UfcAppBarWidget(),
-        ],
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16),
+          child: Column(
+            children: [
+              UfcAppBarWidget(),
+
+              Container(
+                margin: EdgeInsets.only(top: 24),
+                height: 540,
+                color: Colors.blue,
+              ),
+              Expanded(child: Placeholder()),
+            ],
+          ),
+        ),
       ),
     );
   }
