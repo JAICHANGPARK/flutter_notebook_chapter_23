4ff637183c40f96b14cbb2660346d8834641dcd6
Dreamwalker <aristojeff@gmail.com>
2022-10-25 16:55:12 +0900

    Create ufc_appbar_widget.dart

---
+++ lib/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_appbar_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class UfcAppBarWidget extends StatelessWidget {
+  const UfcAppBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
