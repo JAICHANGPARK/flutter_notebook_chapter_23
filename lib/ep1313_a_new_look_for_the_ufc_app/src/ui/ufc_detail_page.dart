9001de57c88ee7bd612fb02061207226b839dfc0
Dreamwalker <aristojeff@gmail.com>
2022-10-25 16:59:31 +0900

    divide label widget

--- lib/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_detail_page.dart
+++ lib/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_23/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_appbar_widget.dart';
+import 'package:flutter_notebook_chapter_23/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_label_widget.dart';
 
 class UfcDetailPage extends StatefulWidget {
   const UfcDetailPage({Key? key}) : super(key: key);
@@ -19,13 +20,22 @@ class _UfcDetailPageState extends State<UfcDetailPage> {
           child: Column(
             children: [
               UfcAppBarWidget(),
-
               Container(
                 margin: EdgeInsets.only(top: 24),
                 height: 540,
                 color: Colors.blue,
+                child: Column(
+                  children: [
+                    Text("Lightweight Champion"),
+                    Expanded(child: Placeholder()),
+                    UfcLabelWidget(),
+                  ],
+                ),
               ),
-              Expanded(child: Placeholder()),
+              Expanded(
+                  child: Column(
+                children: [],
+              )),
             ],
           ),
         ),
---
+++ lib/ep1313_a_new_look_for_the_ufc_app/src/ui/ufc_label_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class UfcLabelWidget extends StatelessWidget {
+  const UfcLabelWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 64,
+      color: const Color.fromRGBO(208, 9, 8, 1),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Image.network(
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/UFC_logo.svg/1280px-UFC_logo.svg.png",
+          width: 84,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
