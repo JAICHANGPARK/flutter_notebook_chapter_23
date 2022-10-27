49cd3a7a0e3eeeb7301525c43a04febf114aeec7
Dreamwalker <aristojeff@gmail.com>
2022-10-27 18:02:49 +0900

    Create live_score_app.dart

---
+++ lib/ep1317_live_score_app/live_score_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class LiveScoreApp extends StatelessWidget {
+  const LiveScoreApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
