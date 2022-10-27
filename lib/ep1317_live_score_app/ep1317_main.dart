ff9862cdc5b8283c2a595d3b5c8a0b2a24e72cb0
Dreamwalker <aristojeff@gmail.com>
2022-10-27 18:03:58 +0900

    Update ep1317_main.dart

--- lib/ep1317_live_score_app/ep1317_main.dart
+++ lib/ep1317_live_score_app/ep1317_main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_23/ep1317_live_score_app/live_score_app.dart';
 
 main() {
-  runApp(LiveScoreApp());
+  runApp(const LiveScoreApp());
 }
