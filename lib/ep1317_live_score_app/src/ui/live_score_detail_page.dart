236b9110bfb589fbc1e2e9b69803db1e514ab39d
Dreamwalker <aristojeff@gmail.com>
2022-10-29 12:23:34 +0900

    Update live_score_detail_page.dart

--- lib/ep1317_live_score_app/src/ui/live_score_detail_page.dart
+++ lib/ep1317_live_score_app/src/ui/live_score_detail_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 
 class LiveScoreDetailPage extends StatefulWidget {
   const LiveScoreDetailPage({Key? key}) : super(key: key);
@@ -11,6 +11,71 @@ class LiveScoreDetailPage extends StatefulWidget {
 class _LiveScoreDetailPageState extends State<LiveScoreDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.blueGrey,
+                    Colors.blueGrey[600]!,
+                    Colors.blueGrey[400]!,
+                    // Colors.blueGrey[300]!,
+                    Colors.brown[200]!,
+                    Colors.brown[200]!,
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: (){
+                          context.pop();
+                        },
+                        child: CircleAvatar(
+                          radius: 36,
+                          backgroundColor: Colors.white.withOpacity(0.2),
+                          foregroundColor: Colors.white,
+                          child: Icon(Icons.arrow_back),
+                        ),
+                      ),
+                      Expanded(
+                          child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Group C"),
+                          Text("Champion League"),
+                        ],
+                      )),
+                      CircleAvatar(
+                        radius: 36,
+                        backgroundColor: Colors.white.withOpacity(0.2),
+                        foregroundColor: Colors.white,
+                        child: Icon(Icons.notifications_none),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
