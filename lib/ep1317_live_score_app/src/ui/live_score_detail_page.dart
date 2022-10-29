90f516f26179b7f55e301b019c5efbbe874df229
Dreamwalker <aristojeff@gmail.com>
2022-10-29 12:19:24 +0900

    Create live_score_detail_page.dart

---
+++ lib/ep1317_live_score_app/src/ui/live_score_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LiveScoreDetailPage extends StatefulWidget {
+  const LiveScoreDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<LiveScoreDetailPage> createState() => _LiveScoreDetailPageState();
+}
+
+class _LiveScoreDetailPageState extends State<LiveScoreDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
