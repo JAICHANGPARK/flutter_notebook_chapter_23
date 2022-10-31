9a197f2579f824a0a000458ed926687a4820e752
Dreamwalker <aristojeff@gmail.com>
2022-10-31 17:48:11 +0900

    Update ep1321_main.dart

--- lib/ep1321_modal_components/ep1321_main.dart
+++ lib/ep1321_modal_components/ep1321_main.dart
@@ -10,26 +10,56 @@ class ModalComponentsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Model Components"),
-        ),
-        body: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => Container(
-                    height: MediaQuery.of(context).size.height,
-                    width: MediaQuery.of(context).size.width,
+      home: ModelComponentPage(),
+    );
+  }
+}
+
+class ModelComponentPage extends StatefulWidget {
+  const ModelComponentPage({Key? key}) : super(key: key);
+
+  @override
+  State<ModelComponentPage> createState() => _ModelComponentPageState();
+}
+
+class _ModelComponentPageState extends State<ModelComponentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Model Components"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text("Test"),
+                ),
+              );
+            },
+            child: Text("Defalt Alert Dialog"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
                   ),
-                );
-              },
-              child: Text("show modal"),
-            ),
-          ],
-        ),
+                ),
+              );
+            },
+            child: Text("show modal"),
+          ),
+        ],
       ),
     );
   }
