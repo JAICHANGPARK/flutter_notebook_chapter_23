e862a4b4e8a896f64737611e359db5501190a66b
Dreamwalker <aristojeff@gmail.com>
2022-10-31 17:41:57 +0900

    Update ep1321_main.dart

---
+++ lib/ep1321_modal_components/ep1321_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(ModalComponentsApp());
+}
+
+class ModalComponentsApp extends StatelessWidget {
+  const ModalComponentsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
