8d96d160dfb0cc1ef8de28a3abf69d73d5b97785
Dreamwalker <aristojeff@gmail.com>
2022-11-02 20:14:29 +0900

    Update feature_sign_up_widget.dart

--- lib/ep1321_modal_components/components/feature_sign_up_widget.dart
+++ lib/ep1321_modal_components/components/feature_sign_up_widget.dart
@@ -12,45 +12,59 @@ class _FeatureSignUpWidgetState extends State<FeatureSignUpWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const Text("Features"),
-            Row(
-              children: [
-                Container(
-                  height: 64,
-                  width: 64,
-                  decoration: const BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Features",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 64,
+                      width: 64,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Text("Getting started"),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.blue[100],
-                              borderRadius: BorderRadius.circular(2),
-                            ),
-                            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                            child: const Text("New"),
+                          Row(
+                            children: [
+                              const Text("Getting started"),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.blue[100],
+                                  borderRadius: BorderRadius.circular(2),
+                                ),
+                                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                                child: const Text("New"),
+                              ),
+                            ],
                           ),
+                          Text(
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+                            overflow: TextOverflow.ellipsis,
+                          )
                         ],
                       ),
-                      Text(
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-                        overflow: TextOverflow.ellipsis,
-                      )
-                    ],
-                  ),
-                )
-              ],
-            )
-          ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
