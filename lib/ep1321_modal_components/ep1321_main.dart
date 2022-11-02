b5bebc49abca2a29d7f301948dbbfa2a57481e1e
Dreamwalker <aristojeff@gmail.com>
2022-11-02 20:05:19 +0900

    Update ep1321_main.dart

--- lib/ep1321_modal_components/ep1321_main.dart
+++ lib/ep1321_modal_components/ep1321_main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_notebook_chapter_23/ep1321_modal_components/components/share_project_widget.dart';
 
 main() {
   runApp(const ModalComponentsApp());
