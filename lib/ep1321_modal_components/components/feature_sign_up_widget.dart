aa7d76342c81edd5ae6c7f7d1b517823a734f331
Dreamwalker <aristojeff@gmail.com>
2022-11-02 20:07:24 +0900

    Create feature_sign_up_widget.dart

---
+++ lib/ep1321_modal_components/components/feature_sign_up_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class FeatureSignUpWidget extends StatefulWidget {
+  const FeatureSignUpWidget({Key? key}) : super(key: key);
+
+  @override
+  State<FeatureSignUpWidget> createState() => _FeatureSignUpWidgetState();
+}
+
+class _FeatureSignUpWidgetState extends State<FeatureSignUpWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
