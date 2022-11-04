89dd4e1c318a344c2dc5d3aca627cead7b29b36a
Dreamwalker <aristojeff@gmail.com>
2022-11-04 18:00:32 +0900

    Update ep1325_main.dart

--- lib/ep1325_bus_ticket_booking_app/ep1325_main.dart
+++ lib/ep1325_bus_ticket_booking_app/ep1325_main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_23/ep1325_bus_ticket_booking_app/bus_ticket_booking_app.dart';
 
 main() {
-  runApp();
+  runApp(const BusTicketBookingApp());
 }
