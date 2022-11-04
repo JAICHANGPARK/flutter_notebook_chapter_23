a4d649d173e19a7dee918e06a11b6d3822835116
Dreamwalker <aristojeff@gmail.com>
2022-11-04 18:04:50 +0900

    Update bus_booking_main_page.dart

--- lib/ep1325_bus_ticket_booking_app/src/ui/bus_booking_main_page.dart
+++ lib/ep1325_bus_ticket_booking_app/src/ui/bus_booking_main_page.dart
@@ -11,28 +11,32 @@ class _BusBookingMainPageState extends State<BusBookingMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.red,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.search,
+      bottomNavigationBar: SizedBox(
+        height: 80,
+        child: BottomNavigationBar(
+          elevation: 4,
+          selectedItemColor: Colors.red,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+              ),
+              label: "Booking",
             ),
-            label: "Booking",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.confirmation_number,
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.confirmation_number,
+              ),
+              label: "Tickets",
             ),
-            label: "Tickets",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person_2_outlined,
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_2_outlined,
+              ),
+              label: "Profile",
             ),
-            label: "Profile",
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
