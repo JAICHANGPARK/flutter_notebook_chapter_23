fd0dd4cfe42e2be966447119b3579c6882181ca1
Dreamwalker <aristojeff@gmail.com>
2022-11-04 18:07:07 +0900

    Update bus_booking_main_page.dart

--- lib/ep1325_bus_ticket_booking_app/src/ui/bus_booking_main_page.dart
+++ lib/ep1325_bus_ticket_booking_app/src/ui/bus_booking_main_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final busTicketAppMenuIndex = StateProvider<int>((ref) => 0);
 
 class BusBookingMainPage extends StatefulWidget {
   const BusBookingMainPage({Key? key}) : super(key: key);
@@ -8,37 +11,41 @@ class BusBookingMainPage extends StatefulWidget {
 }
 
 class _BusBookingMainPageState extends State<BusBookingMainPage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: SizedBox(
-        height: 80,
-        child: BottomNavigationBar(
-          elevation: 4,
-          selectedItemColor: Colors.red,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.search,
+    return Consumer(builder: (context, ref, _) {
+      final index = ref.watch(busTicketAppMenuIndex);
+      return Scaffold(
+        bottomNavigationBar: SizedBox(
+          height: 80,
+          child: BottomNavigationBar(
+            elevation: 4,
+            selectedItemColor: Colors.red,
+            currentIndex: index,
+            onTap: (idx) => ref.read(busTicketAppMenuIndex.notifier).state = idx,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                ),
+                label: "Booking",
               ),
-              label: "Booking",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.confirmation_number,
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.confirmation_number,
+                ),
+                label: "Tickets",
               ),
-              label: "Tickets",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.person_2_outlined,
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_2_outlined,
+                ),
+                label: "Profile",
               ),
-              label: "Profile",
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
