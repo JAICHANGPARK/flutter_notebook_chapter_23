1c77b16a07018a253cff49631a452da533e8de67
Dreamwalker <aristojeff@gmail.com>
2022-11-05 10:44:44 +0900

    Create bus_booking_home_screen.dart

---
+++ lib/ep1325_bus_ticket_booking_app/src/ui/bus_booking_home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class BusBookingHomeScreen extends StatefulWidget {
+  const BusBookingHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BusBookingHomeScreen> createState() => _BusBookingHomeScreenState();
+}
+
+class _BusBookingHomeScreenState extends State<BusBookingHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
