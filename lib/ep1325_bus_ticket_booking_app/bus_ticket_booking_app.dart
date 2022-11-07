6ccd9587b331edbb074e27141a7e062e07d7ffae
Dreamwalker <aristojeff@gmail.com>
2022-11-07 17:38:13 +0900

    Update bus_ticket_booking_app.dart

--- lib/ep1325_bus_ticket_booking_app/bus_ticket_booking_app.dart
+++ lib/ep1325_bus_ticket_booking_app/bus_ticket_booking_app.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_23/ep1325_bus_ticket_booking_app/src/ui/bus_booking_detail_page.dart';
 import 'package:flutter_notebook_chapter_23/ep1325_bus_ticket_booking_app/src/ui/bus_booking_main_page.dart';
 import 'package:go_router/go_router.dart';
 
 class BusTicketBookingApp extends StatelessWidget {
   BusTicketBookingApp({Key? key}) : super(key: key);
-  final appRoute = GoRouter(routes: [
-    GoRoute(
-      path: "/",
-      builder: (context, state) => const BusBookingMainPage(),
-    ),
-  ]);
+  final appRoute = GoRouter(
+    routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) => const BusBookingMainPage(),
+        routes: [
+          GoRoute(
+            path: "detail",
+            builder: (context, state) => BusBookingDetailPage(),
+          ),
+        ],
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
