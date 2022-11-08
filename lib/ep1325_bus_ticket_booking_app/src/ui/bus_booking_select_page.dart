bd5c078e6885b8a17804ba15271e4afdf69a54c2
Dreamwalker <aristojeff@gmail.com>
2022-11-08 20:48:05 +0900

    Create bus_booking_select_page.dart

---
+++ lib/ep1325_bus_ticket_booking_app/src/ui/bus_booking_select_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BusBookingSelectPage extends StatefulWidget {
+  const BusBookingSelectPage({Key? key}) : super(key: key);
+
+  @override
+  State<BusBookingSelectPage> createState() => _BusBookingSelectPageState();
+}
+
+class _BusBookingSelectPageState extends State<BusBookingSelectPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
