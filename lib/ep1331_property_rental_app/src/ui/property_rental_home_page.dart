bbd88c4e37e43d065e5351f66137dbd7cfa080ba
Dreamwalker <aristojeff@gmail.com>
2022-11-10 17:38:43 +0900

    Create property_rental_home_page.dart

---
+++ lib/ep1331_property_rental_app/src/ui/property_rental_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PropertyRentalHomePage extends StatefulWidget {
+  const PropertyRentalHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<PropertyRentalHomePage> createState() => _PropertyRentalHomePageState();
+}
+
+class _PropertyRentalHomePageState extends State<PropertyRentalHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
