1a1b3ef3ab816fbcd0f948ededffb9cff8ddbee9
Dreamwalker <aristojeff@gmail.com>
2022-11-10 17:36:46 +0900

    Create property_rental_app.dart

---
+++ lib/ep1331_property_rental_app/property_rental_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PropertyRentalApp extends StatelessWidget {
+  const PropertyRentalApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
