9237425276bded6714164936331c41afc6759715
Dreamwalker <aristojeff@gmail.com>
2022-11-10 17:37:22 +0900

    Update ep1331_main.dart

---
+++ lib/ep1331_property_rental_app/ep1331_main.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_23/ep1331_property_rental_app/property_rental_app.dart';
+
+main() {
+  runApp(const PropertyRentalApp());
+}
