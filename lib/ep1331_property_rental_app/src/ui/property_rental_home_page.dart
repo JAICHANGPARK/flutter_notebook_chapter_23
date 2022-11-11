b822736441a9ba2cc955198f925bf65f4bfab1a6
Dreamwalker <aristojeff@gmail.com>
2022-11-11 16:23:48 +0900

    Update property_rental_home_page.dart

--- lib/ep1331_property_rental_app/src/ui/property_rental_home_page.dart
+++ lib/ep1331_property_rental_app/src/ui/property_rental_home_page.dart
@@ -61,17 +61,54 @@ class _PropertyRentalHomePageState extends State<PropertyRentalHomePage> {
                     height: 16,
                   ),
                   Container(
-                    height: 240,
+                    height: 210,
                     color: Colors.pink,
                     child: ListView.builder(
                       scrollDirection: Axis.horizontal,
                       itemCount: 10,
                       itemBuilder: (context, index) {
                         return Container(
-                          width: MediaQuery.of(context).size.width / 1.5,
+                          width: MediaQuery.of(context).size.width / 1.6,
                           margin: const EdgeInsets.only(right: 16),
                           decoration: const BoxDecoration(
                             color: Colors.orange,
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 130,
+                                decoration: BoxDecoration(
+                                  color: Colors.blueGrey,
+                                ),
+                              ),
+                              SizedBox(height: 8,),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+
+                                children: [
+                                  Text("Treasure Cove"),
+
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 4),
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.location_on,
+                                          color: Colors.grey,
+                                        ),
+                                        Text("449. Black B/136,Arenja Corner Hotel")
+                                      ],
+                                    ),
+                                  ),
+                                  Text(
+                                    "\$1,000,000/month",
+                                    style: TextStyle(
+                                      color: Color(0xff6e38e0),
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
                           ),
                         );
                       },
