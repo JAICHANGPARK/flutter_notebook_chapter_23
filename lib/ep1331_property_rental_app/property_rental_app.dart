16525d5f97d9ed1ef7e7c900b7b4366f9b72719e
Dreamwalker <aristojeff@gmail.com>
2022-11-12 11:47:32 +0900

    Update property_rental_app.dart

--- lib/ep1331_property_rental_app/property_rental_app.dart
+++ lib/ep1331_property_rental_app/property_rental_app.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_23/ep1331_property_rental_app/src/ui/property_rental_home_page.dart';
 import 'package:go_router/go_router.dart';
 
+import 'src/ui/property_rental_detail_page.dart';
+
 class PropertyRentalApp extends StatelessWidget {
   PropertyRentalApp({Key? key}) : super(key: key);
   final _router = GoRouter(routes: [
@@ -12,6 +14,11 @@ class PropertyRentalApp extends StatelessWidget {
               builder: (context, state) {
                 return const PropertyRentalHomePage();
               }),
+          GoRoute(
+              path: "detail",
+              builder: (context, state) {
+                return const PropertyRentalDetailPage();
+              })
         ],
         builder: (context, state, child) {
           return const PropertyRentalHomePage();
