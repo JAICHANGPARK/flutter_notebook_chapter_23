f8b71579b5f6383c75fb8c5fe3fcdf90130a6dd8
Dreamwalker <aristojeff@gmail.com>
2022-11-13 11:01:14 +0900

    Update property_rental_detail_page.dart

--- lib/ep1331_property_rental_app/src/ui/property_rental_detail_page.dart
+++ lib/ep1331_property_rental_app/src/ui/property_rental_detail_page.dart
@@ -143,12 +143,14 @@ class _PropertyRentalDetailPageState extends State<PropertyRentalDetailPage> {
                         ),
                         const Text(
                           "About",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18
-                          ),
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                         ),
-                        const Text("lorem"),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        const Text(
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        ),
                         Divider(
                           color: Colors.grey[500],
                           height: 42,
