0cde42c9d969a2cc0c45ec330315d11b0ab432e9
Dreamwalker <aristojeff@gmail.com>
2022-11-13 11:01:37 +0900

    Update property_rental_detail_page.dart

--- lib/ep1331_property_rental_app/src/ui/property_rental_detail_page.dart
+++ lib/ep1331_property_rental_app/src/ui/property_rental_detail_page.dart
@@ -148,8 +148,11 @@ class _PropertyRentalDetailPageState extends State<PropertyRentalDetailPage> {
                         SizedBox(
                           height: 16,
                         ),
-                        const Text(
+                        Text(
                           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                          style: TextStyle(
+                            color: Colors.grey[200]!,
+                          ),
                         ),
                         Divider(
                           color: Colors.grey[500],
