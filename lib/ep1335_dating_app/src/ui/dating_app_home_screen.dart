6f2b5468e0c51ac0a75226d9ca72020c96ec7098
Dreamwalker <aristojeff@gmail.com>
2022-11-14 16:43:03 +0900

    Update dating_app_home_screen.dart

---
+++ lib/ep1335_dating_app/src/ui/dating_app_home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DatingAppHomeScreen extends StatefulWidget {
+  const DatingAppHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DatingAppHomeScreen> createState() => _DatingAppHomeScreenState();
+}
+
+class _DatingAppHomeScreenState extends State<DatingAppHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
