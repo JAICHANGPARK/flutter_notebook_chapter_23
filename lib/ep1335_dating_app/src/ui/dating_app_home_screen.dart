d2c3030b035636b47d4045a0496a3123ffeb370c
Dreamwalker <aristojeff@gmail.com>
2022-11-14 16:46:40 +0900

    Update dating_app_home_screen.dart

--- lib/ep1335_dating_app/src/ui/dating_app_home_screen.dart
+++ lib/ep1335_dating_app/src/ui/dating_app_home_screen.dart
@@ -8,12 +8,40 @@ class DatingAppHomeScreen extends StatefulWidget {
 }
 
 class _DatingAppHomeScreenState extends State<DatingAppHomeScreen> {
+  Color bottomPurpleColor = Color.fromRGBO(190, 161, 255, 1);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 80,
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.home_filled,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(child: Container()),
+              Expanded(child: Container()),
+              Expanded(child: Container()),
+            ],
+          ),
         ),
       ),
     );
