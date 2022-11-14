c4398bd8929ac90378fc59bb229ddf70fe0f2f57
Dreamwalker <aristojeff@gmail.com>
2022-11-14 16:43:05 +0900

    Update dating_app.dart

--- lib/ep1335_dating_app/dating_app.dart
+++ lib/ep1335_dating_app/dating_app.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_23/ep1335_dating_app/src/ui/dating_app_home_screen.dart';
+import 'package:go_router/go_router.dart';
 
 class DatingApp extends StatelessWidget {
   const DatingApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp.router(
+      routerConfig: GoRouter(
+        routes: [
+          GoRoute(
+            path: "/",
+            builder: (context, state) => DatingAppHomeScreen(),
+          ),
+        ],
+      ),
+    );
   }
 }
