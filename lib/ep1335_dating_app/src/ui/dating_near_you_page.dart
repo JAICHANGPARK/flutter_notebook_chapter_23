34e49021b2ec7179e1b573336d8b59cd6e81fe49
Dreamwalker <aristojeff@gmail.com>
2022-11-17 19:21:20 +0900

    Create dating_near_you_page.dart

---
+++ lib/ep1335_dating_app/src/ui/dating_near_you_page.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+
+class DatingNearYouPage extends StatefulWidget {
+  const DatingNearYouPage({Key? key}) : super(key: key);
+
+  @override
+  State<DatingNearYouPage> createState() => _DatingNearYouPageState();
+}
+
+class _DatingNearYouPageState extends State<DatingNearYouPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
