e793944cc1a9ddd9503be1f6cc46f5dd766622d6
Dreamwalker <aristojeff@gmail.com>
2022-11-19 22:38:04 +0900

    Update sales_reports_home_page.dart

--- lib/ep1340_sales_reports_app/src/ui/sales_reports_home_page.dart
+++ lib/ep1340_sales_reports_app/src/ui/sales_reports_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SalesReportHomePage extends StatefulWidget {
   const SalesReportHomePage({Key? key}) : super(key: key);
 
@@ -12,10 +11,38 @@ class _SalesReportHomePageState extends State<SalesReportHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 28,
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(48),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: "Search your data",
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
