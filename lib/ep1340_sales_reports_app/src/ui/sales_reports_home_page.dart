ab18e5ad76c442c0197691653c84759772786e71
Dreamwalker <aristojeff@gmail.com>
2022-11-19 22:41:12 +0900

    Update sales_reports_home_page.dart

--- lib/ep1340_sales_reports_app/src/ui/sales_reports_home_page.dart
+++ lib/ep1340_sales_reports_app/src/ui/sales_reports_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SalesReportHomePage extends StatefulWidget {
@@ -14,14 +15,17 @@ class _SalesReportHomePageState extends State<SalesReportHomePage> {
       body: SafeArea(
         child: Column(
           children: [
+            const SizedBox(
+              height: 16,
+            ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
               child: Row(
                 children: [
-                  CircleAvatar(
+                  const CircleAvatar(
                     radius: 28,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 16,
                   ),
                   Expanded(
@@ -30,16 +34,56 @@ class _SalesReportHomePageState extends State<SalesReportHomePage> {
                         border: Border.all(),
                         borderRadius: BorderRadius.circular(48),
                       ),
-                      child: TextField(
+                      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                      child: const TextField(
                         decoration: InputDecoration(
                           hintText: "Search your data",
+                          icon: Icon(
+                            CupertinoIcons.search,
+                          ),
+                          border: InputBorder.none,
                         ),
                       ),
-                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
                     ),
                   ),
                 ],
               ),
+            ),
+            const Divider(
+              endIndent: 16,
+              indent: 16,
+              height: 32,
+            ),
+            Text("Create"),
+            Text("New Report"),
+            Container(),
+            Container(),
+            Row(
+              children: [
+                Expanded(child: Container()),
+                Expanded(child: Container()),
+              ],
+            ),
+            Container(
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            const Divider(
+              endIndent: 16,
+              indent: 16,
+              height: 32,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Lastest Reports"),
+                TextButton(
+                  onPressed: () {},
+                  child: Text("View all"),
+                )
+              ],
             )
           ],
         ),
