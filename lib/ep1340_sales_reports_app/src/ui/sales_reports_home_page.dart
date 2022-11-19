6ad8ca3f6f1da784ee6b4da2b0ab9b3f6cb7e96c
Dreamwalker <aristojeff@gmail.com>
2022-11-19 22:31:18 +0900

    Update sales_reports_home_page.dart

---
+++ lib/ep1340_sales_reports_app/src/ui/sales_reports_home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class SalesReportHomePage extends StatefulWidget {
+  const SalesReportHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<SalesReportHomePage> createState() => _SalesReportHomePageState();
+}
+
+class _SalesReportHomePageState extends State<SalesReportHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
