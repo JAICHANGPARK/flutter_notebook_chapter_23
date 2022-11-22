acca719b0fad6ec27065460fac5cf4f343a14071
Dreamwalker <aristojeff@gmail.com>
2022-11-22 16:59:35 +0900

    Update taxi_profile_page.dart

---
+++ lib/ep1343_taxi_driver_profile_app/src/taxi_profile_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class TaxiProfilePage extends StatefulWidget {
+  const TaxiProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<TaxiProfilePage> createState() => _TaxiProfilePageState();
+}
+
+class _TaxiProfilePageState extends State<TaxiProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
+
