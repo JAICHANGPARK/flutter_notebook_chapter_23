65fe604d7455d81046dd4dd38c99ed4bf63a5db9
Dreamwalker <aristojeff@gmail.com>
2022-11-22 17:00:37 +0900

    Update taxi_profile_app.dart

--- lib/ep1343_taxi_driver_profile_app/src/taxi_profile_app.dart
+++ lib/ep1343_taxi_driver_profile_app/src/taxi_profile_app.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_23/ep1343_taxi_driver_profile_app/src/taxi_profile_page.dart';
 
 class TaxiProfileApp extends StatelessWidget {
   const TaxiProfileApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
+    return const MaterialApp(
+      home: TaxiProfilePage(),
     );
   }
 }
