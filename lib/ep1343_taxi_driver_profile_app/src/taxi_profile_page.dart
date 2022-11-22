d6c26a7d94a1376a0d74dfefe6e1c9768df3e326
Dreamwalker <aristojeff@gmail.com>
2022-11-22 17:06:44 +0900

    Update taxi_profile_page.dart

--- lib/ep1343_taxi_driver_profile_app/src/taxi_profile_page.dart
+++ lib/ep1343_taxi_driver_profile_app/src/taxi_profile_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TaxiProfilePage extends StatefulWidget {
   const TaxiProfilePage({Key? key}) : super(key: key);
 
@@ -12,8 +11,32 @@ class _TaxiProfilePageState extends State<TaxiProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        centerTitle: true,
+        title: Column(
+          children: [
+            Text(
+              "Driving Profile",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              "Updated today at 5:03",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
