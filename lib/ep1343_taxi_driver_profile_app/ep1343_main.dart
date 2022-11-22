484276ab5e89ecbd08e19de05c59cd391f7ced94
Dreamwalker <aristojeff@gmail.com>
2022-11-22 17:03:26 +0900

    Update ep1343_main.dart

--- lib/ep1343_taxi_driver_profile_app/ep1343_main.dart
+++ lib/ep1343_taxi_driver_profile_app/ep1343_main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_23/ep1343_taxi_driver_profile_app/src/taxi_profile_app.dart';
 
 main() {
-  runApp(TaxiProfileApp());
+  runApp(const TaxiProfileApp());
 }
