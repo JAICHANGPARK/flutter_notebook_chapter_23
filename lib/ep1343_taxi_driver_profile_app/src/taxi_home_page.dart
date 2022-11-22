fa9d451952828bfd8a168cf494eb358981eb08ff
Dreamwalker <aristojeff@gmail.com>
2022-11-22 17:04:41 +0900

    Update taxi_home_page.dart

--- lib/ep1343_taxi_driver_profile_app/src/taxi_home_page.dart
+++ lib/ep1343_taxi_driver_profile_app/src/taxi_home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_23/ep1343_taxi_driver_profile_app/src/taxi_profile_page.dart';
 
 class TaxiHomePage extends StatelessWidget {
   const TaxiHomePage({Key? key}) : super(key: key);
@@ -7,12 +7,20 @@ class TaxiHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Taxi App'),
+      ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             GestureDetector(
-              onTap: (){},
-              child: CircleAvatar(
+              onTap: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const TaxiProfilePage(),
+                ),
+              ),
+              child: const CircleAvatar(
                 radius: 64,
                 child: Text("Profile"),
               ),
