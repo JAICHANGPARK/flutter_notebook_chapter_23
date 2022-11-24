f4c5efaa32e148c9d47e082738231f6b5b86d48f
Dreamwalker <aristojeff@gmail.com>
2022-11-24 17:43:09 +0900

    Update shopink_app.dart

--- lib/ep1345_shopink_app/src/shopink_app.dart
+++ lib/ep1345_shopink_app/src/shopink_app.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_23/ep1345_shopink_app/src/ui/shopink_home_page.dart';
 import 'package:go_router/go_router.dart';
 
 class ShopinkApp extends StatelessWidget {
-   ShopinkApp({Key? key}) : super(key: key);
+  ShopinkApp({Key? key}) : super(key: key);
 
   final _router = GoRouter(routes: [
     GoRoute(
       path: "/",
+      builder: (context, state) => const ShopinkHomePage(),
     ),
   ]);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router();
+    return MaterialApp.router(
+      routerConfig: _router,
+    );
   }
 }
