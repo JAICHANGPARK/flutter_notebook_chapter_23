4619145068a188b8a474c906b186f237d54f2723
Dreamwalker <aristojeff@gmail.com>
2022-11-24 17:41:09 +0900

    Create shopink_home_page.dart

---
+++ lib/ep1345_shopink_app/src/ui/shopink_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ShopinkHomePage extends StatefulWidget {
+  const ShopinkHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<ShopinkHomePage> createState() => _ShopinkHomePageState();
+}
+
+class _ShopinkHomePageState extends State<ShopinkHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
