051f57faef556b9d938ef8b71376581a79e2d24f
Dreamwalker <aristojeff@gmail.com>
2022-11-24 17:46:39 +0900

    Update shopink_home_page.dart

--- lib/ep1345_shopink_app/src/ui/shopink_home_page.dart
+++ lib/ep1345_shopink_app/src/ui/shopink_home_page.dart
@@ -11,7 +11,41 @@ class _ShopinkHomePageState extends State<ShopinkHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 26,
+                    child: Icon(
+                      Icons.menu,
+                    ),
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.grey[300],
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        "Shopink",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  CircleAvatar(
+                    radius: 26,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
