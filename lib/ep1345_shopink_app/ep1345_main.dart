eb9021a1c16460d3d385123d889b815e0ae5cc28
Dreamwalker <aristojeff@gmail.com>
2022-11-25 16:59:53 +0900

    Update ep1345_main.dart

--- lib/ep1345_shopink_app/ep1345_main.dart
+++ lib/ep1345_shopink_app/ep1345_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_23/ep1345_shopink_app/src/shopink_app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
-  runApp(ShopinkApp());
+  runApp(ProviderScope(child: ShopinkApp()));
 }
