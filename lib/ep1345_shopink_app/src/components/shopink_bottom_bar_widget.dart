1edf9a9b9d7ed7e957d15844bbd9f39bbc1b906b
Dreamwalker <aristojeff@gmail.com>
2022-11-25 17:06:25 +0900

    Create shopink_bottom_bar_widget.dart

---
+++ lib/ep1345_shopink_app/src/components/shopink_bottom_bar_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ShopinkBottomBarWidget extends ConsumerWidget {
+  const ShopinkBottomBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ref) {
+    return Container();
+  }
+}
