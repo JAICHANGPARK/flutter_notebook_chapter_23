ea98eaa18e7e626b5e60203c910ebc41fa8865bb
Dreamwalker <aristojeff@gmail.com>
2022-11-27 18:09:03 +0900

    Update shopink_detail_page.dart

--- lib/ep1345_shopink_app/src/ui/shopink_detail_page.dart
+++ lib/ep1345_shopink_app/src/ui/shopink_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ShopinkDetailPage extends StatefulWidget {
   const ShopinkDetailPage({Key? key}) : super(key: key);
 
@@ -11,6 +10,34 @@ class ShopinkDetailPage extends StatefulWidget {
 class _ShopinkDetailPageState extends State<ShopinkDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  radius: 24,
+                  child: Icon(
+                    Icons.keyboard_arrow_left_sharp,
+                  ),
+                ),
+                Expanded(child: Center(child: Text("Details Products"),)),
+                CircleAvatar(
+                  radius: 24,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  child: Icon(
+                    Icons.more_horiz,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
