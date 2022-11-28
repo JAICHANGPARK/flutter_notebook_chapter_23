a124cea5911a0777f3cb1ec12abc71b22e77ad3a
Dreamwalker <aristojeff@gmail.com>
2022-11-28 17:18:51 +0900

    Update shopink_controller.dart

--- lib/ep1345_shopink_app/src/controller/shopink_controller.dart
+++ lib/ep1345_shopink_app/src/controller/shopink_controller.dart
@@ -2,3 +2,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final shopinkMenuIndex = StateProvider<int>((ref) => 0);
 final shopinkTabMenuIndex = StateProvider<int>((ref) => 0);
+final shopinkImageIndex = StateProvider<int>((ref) => 0);
